8cf38ff4d174f2059b2ad982884235661f02d429
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-08-09 10:04:38 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORY_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> attendees = new List<string>();
+            string input;
+
+            Console.WriteLine("Enter names of students logging into the online class (type 'exit' to finish)");
+            while (true)
+            {
+                Console.Write("Enter name: ");
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                if (!attendees.Contains(input))
+                {
+                    attendees.Add(input);
+                    Console.WriteLine($"'{input}' logged in.");
+                }
+                else
+                {
+                    Console.WriteLine($"'{input}' has already logged in.");
+                }
+            }
+            attendees.Sort();
+            Console.WriteLine($"\nTotal unique attendees: {attendees.Count}");
+            Console.WriteLine("Unique attendees:");
+            foreach (var name in attendees)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
+             
